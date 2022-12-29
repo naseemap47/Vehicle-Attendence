82c45f9e49a51dd78bdf644b9d0d22976d552f3f
joelkp <joel@eternalrobotics.com>
2022-12-29 18:05:39 +0530

    save number plate image - when new number plate is found and confidence > 0.5

--- detect.py
+++ detect.py
@@ -7,7 +7,7 @@ import easyocr
 import datetime
 
 
-def getAttendance(name, number):
+def getAttendance(name, number, img_roi):
     with open('Attendance.csv', 'r+') as f:
         data = f.readlines()
         name_list = []
@@ -21,25 +21,29 @@ def getAttendance(name, number):
             date_str = c_time.strftime("%d/%m/%Y %H:%M:%S")
             f.writelines(f'{name}, {number}, {date_str}\n')
 
+            # Save Number Plate
+            cv2.imwrite(f"Images/{len(os.listdir('Images'))}.jpg", img_roi)
+
 
 save = True
 confidence = 0.5
 class_labels = 'no_plate'
+os.makedirs('Images', exist_ok=True)
 
 # DeepSort
 tracker = DeepSort(max_age=5)
 
 # YOLOv7
-model = custom(path_or_model='best.pt')
+model = custom(path_or_model='best.pt', gpu=True)
 
-cap = cv2.VideoCapture('Videos/test.mp4')
+cap = cv2.VideoCapture('test.mp4')
 fps = cap.get(cv2.CAP_PROP_FPS)
 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 if save:
     out_vid = cv2.VideoWriter('output.mp4', 
-            cv2.VideoWriter_fourcc(*'MP4V'),
+            cv2.VideoWriter_fourcc(*'mp4v'),
             fps, (w, h))
 
 while True:
@@ -48,8 +52,8 @@ while True:
         break
 
     track_box_list = []
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = model(img_rgb)
+    # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = model(img)
 
     # Bounding Box
     box = results.pandas().xyxy[0]
@@ -61,7 +65,8 @@ while True:
             boxes = [xmin, ymin, int(xmax-xmin), int(ymax-ymin)]
             bbs = (boxes, conf, class_labels[class_id])
             track_box_list.append(bbs)
-
+            # plot_one_box([xmin, ymin, xmax, ymax], img, (0, 150, 0), class_labels, 2)
+    
     if len(track_box_list)>0:
         tracks = tracker.update_tracks(track_box_list, frame=img)
         for track in tracks:
@@ -86,16 +91,20 @@ while True:
             if len(result_ocr)>0:
                 plate_no = result_ocr[0][1].upper()
                 plot_one_box(bbox, img, (0, 150, 0), f'{plate_no}', 4)
-                getAttendance(track_id, plate_no)
-            # cv2.imshow('Video roi', img_roi)
-            # cv2.imwrite(f"Images/{len(os.listdir('Images'))}.jpg", img_roi)
+                if result_ocr[0][2] > 0.5:                    
+                    getAttendance(track_id, plate_no, img_roi)
+            
+            # Save Number Plate Image
+            cv2.imshow('Video roi', img_roi)
             
     if save:
         out_vid.write(img)
-    # cv2.imshow('Video', img)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     cv2.destroyAllWindows()
-    #     break
+
+    img = cv2.resize(img, (940, 550))
+    cv2.imshow('Video', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
 
 if save:
     out_vid.release()
